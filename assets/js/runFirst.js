2e2f843ea04e90f072027121397b9c94879a6ce4
CougarChan <gohan0136@gmail.com>
2022-02-06 15:14:27 -0500

    update

---
+++ assets/js/runFirst.js
@@ -0,0 +1,8 @@
+const apiKimbd = "k_4s8hgj7";
+const apiKwmode = "h2LXbCztIcaw7kZ2ENKOWBrMpS0TnqoOccCqFF58";
+
+let Myfavs = [];
+  if( localStorage.getItem("myMovies")){
+    searchedCities = JSON.parse(localStorage.getItem("citysearch"));
+  }
+
