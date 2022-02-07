460116f2ca49a0020ae3ffe86a191570195f8930
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-02-06 20:07:14 -0500

    edited css style of background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-const mySearch = document.getElementById("#search-text");
+// Initial Values 
+const APIURL = 'https://imdb-api.com/en/API/MostPopularMovies/k_4s8hgj73';
+const IMGPATH = '"https://imdb-api.com/images/original';
+const WatchMod_API_KEY = 'h2LXbCztIcaw7kZ2ENKOWBrMpS0TnqoOccCqFF5';
+const SEARCHAPI = 'https://imdb-api.com/en/API/SearchMovie/k_4s8hgj73';
 
-function movieSearch (){
-    
-}
+
+
+const mySearch = document.getElementById("#search-text");
