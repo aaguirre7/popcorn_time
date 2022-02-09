1d77ea0d6ffcf6495aedd9df190d395dde8ffdb3
aaguirre7 <aguirre.abraham@gmail.com>
2022-02-09 00:12:14 -0500

    popcorn v1.5

--- assets/js/runFirst.js
+++ assets/js/runFirst.js
@@ -1,8 +1,43 @@
-const apiKimbd = "k_4s8hgj73";
+const apiKimbd = "k_x9h3qobd";
 const apiKwmode = "h2LXbCztIcaw7kZ2ENKOWBrMpS0TnqoOccCqFF58";
 
 let Myfavs = [];
   if( localStorage.getItem("myMovies")){
     searchedCities = JSON.parse(localStorage.getItem("citysearch"));
-  }
-
+  } 
+  
+  var container1 = document.getElementById("movieContainer1");
+  var container2 = document.getElementById("movieContainer2");
+  
+  fetch('https://imdb-api.com/en/API/MostPopularMovies/k_x9h3qobd')
+      .then((response) => response.json())
+      .then((data) => {
+          console.log('data:', data);
+          const list = data.items;
+          console.log('list:', list);
+  
+          for (let i = 0; i < 10; i++) {
+            if(i <= 4) {
+              var imgTag = document.createElement("img");
+              imgTag.setAttribute("src", list[i].image);
+              container1.appendChild(imgTag);
+               
+              var pTag = document.createElement("p");
+              pTag.textContent = list[i].title + " (" + list[i].imDbRating + ")";
+              container1.appendChild(pTag);
+              
+            }else {
+              var imgTag = document.createElement("img");
+              imgTag.setAttribute("src", list[i].image);
+              container2.appendChild(imgTag);
+          
+              var pTag = document.createElement("p");
+              pTag.textContent = list[i].title + " (" + list[i].imDbRating + ")";
+              container2.appendChild(pTag);
+              
+            }
+          }
+      })
+      .catch(error => {
+          console.log('error', error);
+      });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,28 @@
 
-var movieDetails =[];
-
 $(document).ready(function(){
-  var movieSearched =function (){
-    var mySearch = "300"; //JSON.stringify(document.getElementById('search-text'));
-    //clears container for use 
-    $("#container").html("");
-
-    $.ajax({url:`https://imdb-api.com/en/API/SearchMovie/#{apiKimbd}/300`,
-
-    success: function(result){
-      console.log(result);
-
-    },
-
-    error: function(xhr, ajaxOptions, thrownError){
-      document.querySelector('a#open-modal').addEventListener('click', function(event) {
+  $("button").click(function(){
+    console.log("this ran");
+    
+      var mySearch = $("input:text").val();
+      console.log(mySearch);
+      $.ajax({url:`https://imdb-api.com/en/API/SearchMovie/${apiKimbd}/${mySearch}`,
+      success: function(result){
+         $("#container").html("");
+        console.log(result);
+      },
+      error:function(){
+        document.querySelector('a#open-modal').addEventListener('click', function(event) {
         event.preventDefault();
-        var modal = document.querySelector('.modal');  // assuming you have only 1
+        var modal = document.querySelector('.modal');
         var html = document.querySelector('html');
         modal.classList.add('is-active');
-        html.classList.add('is-clipped');
-      
+        html.classList.add('is-clipped');      
         modal.querySelector('.modal-background').addEventListener('click', function(e) {
-          e.preventDefault();
-          modal.classList.remove('is-active');
-          html.classList.remove('is-clipped');
-        })
-
-      });
-      }
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+      })})
+    } 
     })
-  };
+  });
 })
-
-//document.querySelector('a#open-modal').addEventListener('click', function(event) {
-//  event.preventDefault();
-//  var modal = document.querySelector('.modal');  // assuming you have only 1
-//  var html = document.querySelector('html');
-//  modal.classList.add('is-active');
-//  html.classList.add('is-clipped');
-
-//  modal.querySelector('.modal-background').addEventListener('click', function(e) {
-//    e.preventDefault();
-//    modal.classList.remove('is-active');
-//    html.classList.remove('is-clipped');
-//  })
