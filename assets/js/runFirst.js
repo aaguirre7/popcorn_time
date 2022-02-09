6f9108b05262a269f2f1c1384d11933ff18df02f
Ajaypal Ghuman <asingh0328@yahoo.com>
2022-02-08 22:28:34 -0500

    updated javascript file

--- assets/js/runFirst.js
+++ assets/js/runFirst.js
@@ -2,22 +2,36 @@ const apiKimbd = "k_4s8hgj73";
 const apiKwmode = "h2LXbCztIcaw7kZ2ENKOWBrMpS0TnqoOccCqFF58";
 
 
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
- 
-fetch('https://imdb-api.com/en/API/MostPopularMovies/k_4s8hgj73', requestOptions)
-  .then(response => response.json())
-  .then(response => {
-    const list = response.data;
+var container1 = document.getElementById("movieContainer1");
+var container2 = document.getElementById("movieContainer2");
 
-    list.map((items) => {
-      const name = items.title;
-      const poster = items.image;
-      const movie = `<li><img src=$`
+fetch('https://imdb-api.com/en/API/MostPopularMovies/k_7hu44uii')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('data:', data);
+        const list = data.items;
+        console.log('list:', list);
+
+        for (let i = 0; i < 10; i++) {
+          if(i <= 4) {
+            var imgTag = document.createElement("img");
+            imgTag.setAttribute("src", list[i].image);
+            container1.appendChild(imgTag);
+
+            var pTag = document.createElement("p");
+            pTag.textContent = list[i].title + " (" + list[i].imDbRating + ")";
+            container1.appendChild(pTag);
+          }else {
+            var imgTag = document.createElement("img");
+            imgTag.setAttribute("src", list[i].image);
+            container2.appendChild(imgTag);
+
+            var pTag = document.createElement("p");
+            pTag.textContent = list[i].title + " (" + list[i].imDbRating + ")";
+            container2.appendChild(pTag);
+          }
+        }
     })
-  })
-  .catch(error => {
-    console.log('error', error);
-  });
+    .catch(error => {
+        console.log('error', error);
+    });
