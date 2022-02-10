261950e28ac45e64d4daaed319fbcb78a9f73895
CougarChan <gohan0136@gmail.com>
2022-02-09 21:55:46 -0500

    update

--- assets/js/runFirst.js
+++ assets/js/runFirst.js
@@ -6,3 +6,29 @@ let Myfavs = [];
     searchedCities = JSON.parse(localStorage.getItem("citysearch"));
   }
 
+  var container = document.getElementById("container");
+  var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+  };
+  
+  fetch('https://imdb-api.com/en/API/MostPopularMovies/k_7hu44uii', requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+          console.log('data:', data);
+          const list = data.items;
+          console.log('list:', list);
+  
+          for (let i = 0; i < 10; i++) {
+              var pTag = document.createElement("p");
+              pTag.textContent = list[i].title + " (" + list[i].imDbRating + ")";
+              container.appendChild(pTag);
+  
+              var imgTag = document.createElement("img");
+              imgTag.setAttribute("src", list[i].image);
+              container.appendChild(imgTag);
+          }
+      })
+      .catch(error => {
+          console.log('error', error);
+      });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,48 @@
-const mySearch = document.getElementById("#search-text");
 
-function movieSearch (){
-    
-}
-import Foundation from  'https://cdn.jsdelivr.net/npm/foundation-sites@6.7.4/dist/js/foundation.min.js';
-const mySearch = document.getElementById("#search-text");
-var movieDetails ="";
+var movieDetails =[];
 
-$(document).();
-$(function(searchNow) {
-  $('.search')
-    .bind('click',function(event){
-      $.ajax({url:`https://imdb-api.com/en/API/SearchMovie/${apiKimbd}/${mySearch}`, success:
-        function(result){
-          movieDetails= result;
-        },error: Document.querySelector('a#open-modal').addEventListener('click', function(event) {
-          event.preventDefault();
-          var modal = document.querySelector('.modal');  // assuming you have only 1
-          var html = document.querySelector('html');
-          modal.classList.add('is-active');
-          html.classList.add('is-clipped');
-        
-          modal.querySelector('.modal-background').addEventListener('click', function(e) {
-            e.preventDefault();
-            modal.classList.remove('is-active');
-            html.classList.remove('is-clipped');
-          });
+$(document).ready(function(){
+  var movieSearched =function (){
+    var mySearch = "300"; //JSON.stringify(document.getElementById('search-text'));
+    //clears container for use 
+    $("#container").html("");
+
+    $.ajax({url:`https://imdb-api.com/en/API/SearchMovie/#{apiKimbd}/300`,
+
+    success: function(result){
+      console.log(result);
+
+    },
+
+    error: function(xhr, ajaxOptions, thrownError){
+      document.querySelector('a#open-modal').addEventListener('click', function(event) {
+        event.preventDefault();
+        var modal = document.querySelector('.modal');  // assuming you have only 1
+        var html = document.querySelector('html');
+        modal.classList.add('is-active');
+        html.classList.add('is-clipped');
+      
+        modal.querySelector('.modal-background').addEventListener('click', function(e) {
+          e.preventDefault();
+          modal.classList.remove('is-active');
+          html.classList.remove('is-clipped');
         })
-    });
-  });
 
+      });
+      }
+    })
+  };
 })
+
+//document.querySelector('a#open-modal').addEventListener('click', function(event) {
+//  event.preventDefault();
+//  var modal = document.querySelector('.modal');  // assuming you have only 1
+//  var html = document.querySelector('html');
+//  modal.classList.add('is-active');
+//  html.classList.add('is-clipped');
+
+//  modal.querySelector('.modal-background').addEventListener('click', function(e) {
+//    e.preventDefault();
+//    modal.classList.remove('is-active');
+//    html.classList.remove('is-clipped');
+//  })
